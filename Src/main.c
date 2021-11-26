3f897cf68eed09143fc307bc689bf59cf6c37dba
jaromir.stanko <stanko.jaromir@gmail.com>
2021-11-26 10:49:06 +0100

    used TIM2 instead of TIM3 (not available for f302r8), changed GPIO for onboard LED

--- Src/main.c
+++ Src/main.c
@@ -98,7 +98,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
-  MX_TIM3_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
--- Src/stm32f3xx_it.c
+++ Src/stm32f3xx_it.c
@@ -199,26 +199,26 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles TIM3 global interrupt.
+  * @brief This function handles TIM2 global interrupt.
   */
-void TIM3_IRQHandler(void)
+void TIM2_IRQHandler(void)
 {
-	if(LL_TIM_IsActiveFlag_UPDATE(TIM3))
+	if(LL_TIM_IsActiveFlag_UPDATE(TIM2))
 	{
-		if(LL_GPIO_IsOutputPinSet(GPIOB, LL_GPIO_PIN_3))
+		if(LL_GPIO_IsOutputPinSet(GPIOB, LL_GPIO_PIN_13))
 		{
-			LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_3);
+			LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_13);
 		}
 		else
 		{
-			LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_3);
+			LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_13);
 		}
 
 		ADC_start_conversion();
 		voltage = ADC_convertedValue2float();
 	}
 
-	LL_TIM_ClearFlag_UPDATE(TIM3);
+	LL_TIM_ClearFlag_UPDATE(TIM2);
 }
 
 /* USER CODE BEGIN 1 */
